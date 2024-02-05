44c30f20be9ad729fba8781aa8e74051769ff376
Dean de Bree <ddebree@gmail.com>
2024-02-05 21:23:25 +0200

    Remove old comments

--- src/strategies/DiagonalOnesStrategy.class.ts
+++ src/strategies/DiagonalOnesStrategy.class.ts
@@ -65,35 +65,3 @@ export class DiagonalOnesStrategy extends AbstractCellWithValueStrategy {
     }
 
 }
-
-// def cellfunc_handle_diagonal_ones(puzzle, row, col):
-//     if puzzle.get_board(row, col) != '1':
-//         return
-//
-//     for dr in [-1, 1]:
-//         for dc in [-1, 1]:
-//             next_row = row + 2 * dr
-//             next_col = col + 2 * dc
-//
-//             if next_row < 0 or next_row >= puzzle.board_height or \
-//                next_col < 0 or next_col >= puzzle.board_width:
-//                 # Went off the edge of the board.
-//                 continue
-//
-//             if puzzle.get_board(next_row, next_col) != '1':
-//                 # Not a diagonal 1.  Skip.
-//                 continue
-//
-//             if puzzle.get_board(row, col - dc) == 'x' and \
-//                puzzle.get_board(row - dr, col) == 'x':
-//                 # 'x' values on outer edges of 1-cell.  Put corresponding 'x'
-//                 # values on outer edges of adjacent diagonal 1-cell.
-//                 puzzle.cond_set_link_to_x(next_row, next_col + dc)
-//                 puzzle.cond_set_link_to_x(next_row + dr, next_col)
-//
-//             elif puzzle.get_board(row, col + dc) == 'x' and \
-//                  puzzle.get_board(row + dr, col) == 'x':
-//                 # 'x' values on inner edges of 1-cell.  Put corresponding 'x'
-//                 # values on inner edges of adjacent diagonal 1-cell.
-//                 puzzle.cond_set_link_to_x(next_row, next_col - dc)
-//                 puzzle.cond_set_link_to_x(next_row - dr, next_col)
